47aacabc756e2e8bec19bed9a96c8b4c78e11190
matheusosp <matheusosp@users.noreply.github.com>
2021-01-26 14:11:49 -0300

    added views and urls

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from .models import Product
+from .forms import ProductModelForm
+
+
+class IndexView(ListView):
+    models = Product
+    template_name = 'index.html'
+    queryset = Product.objects.all()
+    context_object_name = 'products'
+
